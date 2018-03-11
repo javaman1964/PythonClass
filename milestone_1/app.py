a33dd3310ae446c2eb2ba4314382869e5b23ca3d
Jerry Payne <jerrylikestospin@gmail.com>
2018-03-11 14:25:17 -0500

    Initial set of files

--- milestone_1/app.py
+++ milestone_1/app.py
@@ -1,6 +1,12 @@
 # Create and search a list of movies
 
 movies = list()
+search_by = [
+    {"X" : "Exiting search..."},
+    {"N" : "Enter the movie name: "},
+    {"D" : "Enter the directors name: "},
+    {"Y" : "Enter the year the movie premiered: "}
+]
 
 # Prompt for movie info and return a dictionary
 def create_movie():
@@ -8,13 +14,31 @@ def create_movie():
     year = input(f"What year did {name} premier?: " )
     director = input(f"Who directed {name}?: ")
     return {
-        "movie":{name},
-        "year":{year},
-        "director":{director}
+        "movie" : {name},
+        "year" : {year},
+        "director" : {director}
     }
 # Take a movie name, search list and return the dictonary
 def find_movie(name):
     print(name)
+
+def list_movies():
+    for movie in movies:
+        print(movie)
+
+def search_movies():
+    action = input("What do you want to search for, Name of movie (N), Year (Y), Director (D), Exit (X)?: ")
+    if action.upper() == "X":
+        print("Exiting search...")
+    elif action.upper() == "N":
+        name = input("Enter the movie name: ")
+    elif action.upper() == "Y":
+        year = input("Enter the premier year: ")
+    elif action.upper() == "D":
+        director = input("Enter the directors name: ")
+    else:
+        print("Invalid value, exiting search...")
+
 
 print("Welcome to my movie app!!!")
 while True:
@@ -27,9 +51,9 @@ while True:
         movie = create_movie()
         movies.append(movie)
     elif action.upper() == "L":
-        print(movies)
+        list_movies()
     elif action.upper() == "S":
-        print("search")
+        movie = search_movies()
     else:
         print("ERROR: Invalid entry, try again")
 
